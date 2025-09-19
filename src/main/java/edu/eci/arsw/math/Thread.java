ad5a8ad51bd735221bcaae07e2edb255d4c6c67d
Natalia-Espitia <natalia.espitia-e@mail.escuelaing.edu.co>
2025-09-19 08:55:19 -0500

    commit de prueba

--- src/main/java/edu/eci/arsw/math/Thread.java
+++ src/main/java/edu/eci/arsw/math/Thread.java
@@ -2,4 +2,19 @@ package edu.eci.arsw.math;
 
 public class Thread {
 
+    PiDigits piDigits;
+
+    public Thread(PiDigits piDigits) {
+        this.piDigits = piDigits;
+    }
+
+    public void run() {
+        System.out.println(PiDigits.getDigits(0, 10));
+        System.out.println(PiDigits.getDigits(1, 100));
+        System.out.println(PiDigits.getDigits(1, 1000000));
+    }
+
+    
+    
 }
+  
